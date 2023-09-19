b44e1592c95716951cdd3f92aac26231eace69b6
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-09-19 18:42:54 +0300

    HW task 02 done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel;
 using System.Text;
+using System.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ConsoleApp090923
 {
@@ -35,6 +37,21 @@ namespace ConsoleApp090923
             Date = date;
         }
     }
+    internal class JournalComparer : IEqualityComparer<Journal>
+    {
+        public bool Equals(Journal? x, Journal? y)
+        {
+            if (x == null || y == null)
+            {
+                return false;
+            }
+            return x.Name == y.Name && x.Genre.ToLower() == y.Genre.ToLower() && x.NumPages == y.NumPages && x.Date == y.Date;
+        }
+        int IEqualityComparer<Journal>.GetHashCode(Journal obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -50,14 +67,14 @@ namespace ConsoleApp090923
                 new Journal("Журнал 3", "Спорт", 35, "2021"),
                 new Journal("Журнал 4", "Сад та город", 40, "2021"),
                 new Journal("Журнал 5", "Культура", 32, "2021"),
-                new Journal("Журнал 6", "Розваги", 28, "2021"),
+                new Journal("Журнал 6", "Розваги", 28, "2022"),
                 new Journal("Журнал 7", "Футбол", 35, "2021"),
                 new Journal("Журнал 8", "Будівництво", 40, "2021"),
                 new Journal("Журнал 9", "Шахи", 32, "2021"),
-                new Journal("Журнал 10", "каталог", 28, "2021"),
-                new Journal("Журнал 11", "Рибалка", 35, "2021"),
+                new Journal("Авто Журнал 10", "каталог", 28, "2021"),
+                new Journal("Журнал 11", "Рибалка", 35, "2022"),
                 new Journal("Журнал 12", "Дача", 40, "2021"),
-                new Journal("Журнал 13", "Полювання", 32, "2021"),
+                new Journal("Журнал 13", "полювання", 32, "2021"),
                 new Journal("Журнал 14", "Зброя", 28, "2021"),
                 new Journal("Журнал 15", "Сад та город", 22, "1981")
             };
@@ -84,7 +101,73 @@ namespace ConsoleApp090923
                 Console.WriteLine("Результат перевірки - не всі журнали в жанрах «Політика», «Мода» або «Спорт»");
             }
             Console.WriteLine();
-
+            Console.WriteLine("Перевірка, чи є хоча б один журнал у жанрі «Сад та город»");
+            var anyGenre = journals.Any(journals => journals.Genre == "Сад та город");
+            if (anyGenre)
+            {
+                Console.WriteLine("Результат перевірки - є хоча б один журнал у жанрі «Сад та город»");
+            }
+            else
+            {
+                Console.WriteLine("Результат перевірки - немає журналів у жанрі «Сад та город»");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Перевірка, чи є в масиві хоча б один журнал у жанрі «Рибалка»");
+            var anyFishing = journals.Any(journals => journals.Genre == "Рибалка");
+            if (anyFishing)
+            {
+                Console.WriteLine("Результат перевірки - є хоча б один журнал у жанрі «Рибалка»");
+            }
+            else
+            {
+                Console.WriteLine("Результат перевірки - немає журналів у жанрі «Рибалка»");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Перевірка, чи є в масиві журнали про «Полювання»");
+            var containsHunting = journals.Any(journals => journals.Genre.ToLower() == "полювання");
+            if (containsHunting)
+            {
+                Console.WriteLine("Результат перевірки - є журнали про «Полювання»");
+            }
+            else
+            {
+                Console.WriteLine("Результат перевірки - немає журналів про «Полювання»");
+            }
+            Console.WriteLine("\nПеревірка, чи є в масиві журнали про «Полювання» методом Contains");
+            var hunting = new Journal("Журнал 13", "Полювання", 32, "2021");
+            bool hasHunting = journals.Contains(hunting, new JournalComparer());
+            if (hasHunting)
+            {
+                Console.WriteLine("Результат перевірки - є журнали про «Полювання»");
+            }
+            else
+            {
+                Console.WriteLine("Результат перевірки - немає журналів про «Полювання»");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Пошук першого журналу з роком випуску 2022");
+            var firstJournal = journals.FirstOrDefault(journals => journals.Date == "2022");
+            if (firstJournal != null)
+            {
+                Console.WriteLine($"Перший журнал з роком випуску 2022 - {firstJournal.Name}");
+            }
+            else
+            {
+                Console.WriteLine("Немає журналів з роком випуску 2022");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Пошук останнього журналу, назва якого починається зі слова «Авто»");
+            var lastJournal = journals.LastOrDefault(journals => journals.Name.StartsWith("Авто"));
+            if (lastJournal != null)
+            {
+                Console.WriteLine($"Останній журнал, назва якого починається зі слова «Авто» - {lastJournal.Name}");
+            }
+            else
+            {
+                Console.WriteLine("Немає журналів, назва яких починається зі слова «Авто»");
+            }
         }
     }
+
+    
 }
